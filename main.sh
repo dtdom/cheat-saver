685bfe529a926899e9e4eec4aba185a3fd8c8a8d
dtdom <davidtamayo93@gmail.com>
2021-04-14 12:17:56 +0200

    -

--- main.sh
+++ main.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 
-if [ -z "$XDG_DATA_HOME" ]; then localcheat="$HOME/.local/share/navi/cheats/mine/mine.cheat"; else localcheat="$XDG_DATA_HOME/navi/cheats/mine/mine.cheat"; fi
+if [ -z "$XDG_DATA_HOME" ]; then localcheat="$HOME/.local/share/navi/cheats/mine"; else localcheat="$XDG_DATA_HOME/navi/cheats/mine"; fi
 read -p "Type title: " title
 read -p "Type command: " command
-echo "# $title" >> $localcheat
-echo "$command" >> $localcheat
-echo "" >> $localcheat
+echo "# $title" >> $localcheat"/mine.cheat"
+echo "$command" >> $localcheat"/mine.cheat"
+echo "" >> $localcheat"/mine.cheat"
+cd $localcheat
+git add $localcheat"/mine.cheat"
+git commit -m "$title"
+git push origin master
